0288bc4b46071ddf662066c21902648ca06afa64
Lifo123 <alberto_p-@hotmail.com>
2024-05-19 14:50:26 -0500

    gh-pages

--- src/Components/GrafficMatch/Match.jsx
+++ src/Components/GrafficMatch/Match.jsx
@@ -4,11 +4,11 @@ function Match({ pState }) {
     return (
         <div className={`gr-match absolute ${pState === 'top' ? 'gr-top' : 'gr-bot'}`}>
             <div className=''>
-                <p className='pointer'>Team Name</p>
+                <p className='pointer'>Team Name 1</p>
                 <span className='d-flex flex-center'>0</span>
             </div>
             <div className='gr-mt-t'>
-                <p className='pointer'>Team Name</p>
+                <p className='pointer'>Team Name 1</p>
                 <span className='d-flex flex-center'>0</span>
             </div>
             <span className='gr-detail d-flex absolute'></span>
