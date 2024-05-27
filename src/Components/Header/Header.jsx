d41feb4b50639a03c9dd93d257d38250bbc966b1
Lifo123 <alberto_p-@hotmail.com>
2024-05-27 13:06:55 -0500

    Landing Init

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect } from 'react'
 import './Header.css'
 import { Context } from '../../App'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Header() {
     const [Auth, setAuth] = useContext(Context);
@@ -26,10 +26,10 @@ function Header() {
             </div>
             <div className='f-row f-align-center g-20'>
                 <ul className='f-row h-list'>
-                    <li>Home</li>
-                    <li>Features</li>
-                    <li>Community</li>
-                    <li>Blog</li>
+                    <Link to={'/Home'}>Home</Link>
+                    <Link to={'/Features'}>Features</Link>
+                    <Link to={'/Community'}>Community</Link>
+                    <Link to={'/Blog'}>Blog</Link>
                 </ul>
                 {Auth ? null : <span className='btn-primary pointer b-getstar'>Get Started</span>}
                 <label className='f-row user-sign g-10 f-align-center mr-40'>
--- src/Pages/LadingPage/LandingPage.jsx
+++ src/Pages/LadingPage/LandingPage.jsx
@@ -1,13 +1,27 @@
+import './Styles/LadingPage.css'
 import { useContext } from 'react';
-import Header from '../../Components/Header/Header';
 import { Context } from '../../App';
+
+import Header from '../../Components/Header/Header';
 
 function LandingPage() {
   const [Auth, setAuth] = useContext(Context)
   return (
     <>
       <Header />
-      {Auth ? 'Estamos redy' : 'Idk'}
+      <main className='lan-main d-flex absolute f-justify-center g-120 w-75'>
+          <div className='f-col g-30 lan-left'>
+            <h1 className=''>Manage your Tournaments <span>Free</span></h1>
+            <p >Choosing colors or typography for your website? Use the Toolbar below to realize your choices. and manage complete free all tournaments with Admin mode.</p>
+            <div className='f-row g-20'>
+              <span className='btn-primary'>Get Started</span>
+              <span className='btn-secondary'>View Tournaments</span>
+            </div>
+          </div>
+          <div className='d-flex f-center lan-right'>
+            <img src="../../../public/ImgLanding.png" alt="#" />
+          </div>
+      </main>
     </>
   )
 }
