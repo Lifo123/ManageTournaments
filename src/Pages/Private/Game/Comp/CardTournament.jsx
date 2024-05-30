925b2900f43ac7a1f720f83f19f015e9c46b40b8
Lifo123 <alberto_p-@hotmail.com>
2024-05-30 18:34:17 -0500

    Features

--- src/Pages/Private/Game/Comp/CardTournament.jsx
+++ src/Pages/Private/Game/Comp/CardTournament.jsx
@@ -11,7 +11,7 @@ export default function CardTournament({ title, desc, id }) {
     return (
         <li className='gm-list g-10 br-6 pointer' onClick={HandleNavigate}>
             <label className='f-col relative g-5 no-select'>
-                <h2 className='ps-3 pointer hm-font-h2'>{title || 'Game Name'}</h2>
+                <h2 className='ps-3 pointer hm-font-h2'>{title || 'Game Name 1'}</h2>
                 <p className='hm-font-p'>{desc || 'Description'}</p>
             </label>
         </li>
