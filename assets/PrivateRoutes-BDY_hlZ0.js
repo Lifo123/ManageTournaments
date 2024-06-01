1ffca1986d3ada816a18dc1c3c9614744f4f6ea7
Lifo123 <alberto_p-@hotmail.com>
2024-06-01 11:15:27 -0500

    Updates

---
+++ assets/PrivateRoutes-BDY_hlZ0.js
@@ -0,0 +1,1 @@
+import{j as t,N as o,O as r}from"./index-BgaBufsf.js";function i({Auth:e,redirectPath:s="/"}){return e?t.jsx(r,{}):(console.log("U dont have permissions"),t.jsx(o,{to:s,replace:!0}))}export{i as default};
