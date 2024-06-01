5b57bf2f233eac6252491bf5987a132256a5b548
Lifo123 <alberto_p-@hotmail.com>
2024-06-01 11:25:46 -0500

    Updates

---
+++ assets/PrivateRoutes-D6A2VybE.js
@@ -0,0 +1,1 @@
+import{j as t,N as o,O as r}from"./index-B2ZA_JRo.js";function i({Auth:e,redirectPath:s="/"}){return e?t.jsx(r,{}):(console.log("U dont have permissions"),t.jsx(o,{to:s,replace:!0}))}export{i as default};
