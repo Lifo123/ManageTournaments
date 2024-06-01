d46a732a4d7d3d4bb82d01d29e12cf823f193043
Lifo123 <alberto_p-@hotmail.com>
2024-06-01 11:23:04 -0500

    Updates

--- assets/PrivateRoutes-CIvrPnLY.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as o,O as r}from"./index-Bbz8DTyO.js";function i({Auth:e,redirectPath:s="/"}){return e?t.jsx(r,{}):(console.log("U dont have permissions"),t.jsx(o,{to:s,replace:!0}))}export{i as default};
