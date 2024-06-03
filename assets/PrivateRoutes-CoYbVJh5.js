ba33c6fa157a4d25ea68a0792ee2b68118f12246
Lifo123 <alberto_p-@hotmail.com>
2024-06-03 10:33:02 -0500

    Updates

--- assets/PrivateRoutes-CoYbVJh5.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as r,O as o}from"./index-DCb_Y4wD.js";/* empty css                     */function i({Auth:e}){return e?t.jsx(o,{}):(console.log("U dont have permissions"),t.jsx(r,{to:"/error",replace:!0}))}export{i as default};
